4760a08fed9076ec2b5ddc7b62334b6ecb1f6c6e
Brycen-walker <91140977+Brycen-walker@users.noreply.github.com>
2022-07-22 00:10:01 -0500

    Create hacker_surviror_defense.py

---
+++ hacker_surviror_defense.py
@@ -0,0 +1,21 @@
+import time
+import os
+
+
+while True:
+	time.sleep(0.1)
+	os.system("who -u | tr -s \" \" | grep -v \"tty7\" | awk \'{print $6}\' > pids.txt")
+	os.system("who -u | tr -s \" \" | grep -v \"tty7\" | awk \'{print $7}\' | sed \"s/(//g\" | sed \"s/)//g\" > ips.txt")
+
+	with open("pids.txt", "r") as file:
+		pids = file.read().split("\n")
+	with open("ips.txt", "r") as file:
+		ips = file.read().split("\n")
+	pids = pids[:-1]
+	ips = ips[:-1]
+	for i in pids:
+		num = 0
+		os.system(f"kill {pids[num]}")
+		print(f"killed {ips[num]}")
+		num += 1
+	os.system("rm pids.txt; rm ips.txt")
